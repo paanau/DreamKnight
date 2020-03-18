b752d283545bd793bee0d29a5b6b8f39e6173330
Autio <petri.mikael.autio@gmail.com>
2020-03-18 23:36:31 +0000

    Parallax

---
+++ Dream Knight/Assets/Scripts/CamMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+	public float speed = 0.02f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3(1,0,0) * speed);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3(1,0,0) * -speed);
+		}
+	
+	}
+}
+
